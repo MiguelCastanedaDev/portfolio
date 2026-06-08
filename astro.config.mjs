18c22bf9e509a52a0550a094ff5dce51a3986c19
MiguelCastanedaDev <mickeycasta4@gmail.com>
2026-06-07 23:09:53 -0600

    feat: hero, collage, navbar and services section.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,25 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig, fontProviders } from 'astro/config';
+import tailwindcss from "@tailwindcss/vite";
 
 import cloudflare from '@astrojs/cloudflare';
 
 // https://astro.build/config
 export default defineConfig({
-  adapter: cloudflare()
+  adapter: cloudflare(),
+  vite: {
+    plugins: [tailwindcss()],
+  },
+  fonts: [{
+    provider: fontProviders.local(),
+    name: "Geist",
+    cssVariable: "--font-geist",
+    options: {
+      variants: [{
+        src: ['./src/assets/fonts/Geist/Geist-VariableFont_wght.ttf'],
+        weight: '400 500 600 700',
+        style: 'normal'
+      }]
+    }
+  }]
 });
